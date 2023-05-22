6cc036e62ac6dd5347fd3a3bf99ed6d263f7604f
Jens Ramakers <jensramakers@gmail.com>
2023-05-22 13:08:26 +0200

    first

---
+++ ElderEatsAPI/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ElderEatsAPI.Models;
+
+public class Product
+{
+    public long Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string ?Brand { get; set; }
+
+
+    [Column("quantity_in_package")]
+    public string ?QuantityInPackage { get; set; }
+
+    public string ?Barcode { get; set; }
+
+    public string ?Image { get; set; }
+}
