9b40fd41d014a216baf33d9f9e072d872ea50b19
jens <jensramakers@gmail.com>
2023-12-14 21:45:23 +0100

    changed url

--- ElderEatsAPI/Controllers/DeployController.cs
+++ ElderEatsAPI/Controllers/DeployController.cs
@@ -8,13 +8,13 @@ namespace ElderEatsAPI.Controllers;
 public class DeployController : ControllerBase
 {
     private readonly IConfiguration _configuration;
-    
+
     public DeployController(IConfiguration configuration)
     {
         _configuration = configuration;
     }
-    
-    [HttpPost]
+
+    [HttpPost("ToepenReact")]
     public IActionResult DeployReact([FromBody] DeployRequest deployRequest)
     {
         if (deployRequest.secret == null)
@@ -30,13 +30,77 @@ public class DeployController : ControllerBase
 
             return StatusCode(401, ModelState);
         }
-        
-        Deploy();
+
+        GitPull();
+        NpmInstall();
+        NpmBuild();
 
         return Ok();
     }
 
-    private static void Deploy()
+    private static void GitPull()
+    {
+        const string projectPath = "/var/www/toepenreact";
+        const string command = "git pull";
+        const string arguments = "";
+
+        ProcessStartInfo psi = new()
+        {
+            FileName = command,
+            Arguments = arguments,
+            WorkingDirectory = projectPath,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        using Process process = new();
+        process.StartInfo = psi;
+        process.Start();
+
+        string output = process.StandardOutput.ReadToEnd();
+        string error = process.StandardError.ReadToEnd();
+
+        process.WaitForExit();
+
+        Console.WriteLine("Output:\n" + output);
+        Console.WriteLine("Error:\n" + error);
+        Console.WriteLine("Exit Code: " + process.ExitCode);
+    }
+
+    private static void NpmInstall()
+    {
+        const string projectPath = "/var/www/toepenreact";
+        const string npmCommand = "npm i";
+        const string npmArguments = "";
+
+        ProcessStartInfo psi = new()
+        {
+            FileName = npmCommand,
+            Arguments = npmArguments,
+            WorkingDirectory = projectPath,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        using Process process = new();
+        process.StartInfo = psi;
+        process.Start();
+
+        string output = process.StandardOutput.ReadToEnd();
+        string error = process.StandardError.ReadToEnd();
+
+        process.WaitForExit();
+
+        Console.WriteLine("Output:\n" + output);
+        Console.WriteLine("Error:\n" + error);
+        Console.WriteLine("Exit Code: " + process.ExitCode);
+    }
+
+    private static void NpmBuild()
     {
         const string projectPath = "/var/www/toepenreact";
         const string npmCommand = "npm";
